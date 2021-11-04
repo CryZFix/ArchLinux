61691aacfd05e0234009f813dabbb9feca68fffd
CryZFix <cryzfix@gmail.com>
2021-11-04 18:51:59 +0500

    Add files via upload

---
+++ arch2.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+ln -svf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
+
+echo '3.4 Добавляем русскую локаль системы'
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+
+echo 'Обновим текущую локаль системы'
+locale-gen
+
+echo 'Указываем язык системы'
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+
+echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+echo 'KEYMAP=ru' >> /etc/vconsole.conf
+echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+
+echo 'Создадим загрузочный RAM диск'
+mkinitcpio -p linux
+
+echo '3.5 Устанавливаем загрузчик'
+pacman -Syy
+pacman -S grub --noconfirm 
+grub-install /dev/sda
+
+echo 'Обновляем grub.cfg'
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Ставим программу для Wi-fi'
+pacman -S dialog wpa_supplicant --noconfirm 
+
+echo 'Добавляем пользователя'
+useradd -m -g users -G wheel -s /bin/bash user
+
+echo 'Создаем root пароль'
+passwd
+
+echo 'Устанавливаем пароль пользователя'
+passwd user
+
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+echo 'Ставим иксы и драйвера'
+pacman -S xorg-server xorg-drivers xorg-xinit virtualbox-guest-utils lib32-mesa-libgl wget
+
+echo "Ставим XFCE"
+pacman -S xfce4 xfce4-goodies --noconfirm
+
+echo 'Cтавим DM'
+pacman -S sddm --noconfirm
+systemctl enable sddm
+
+echo 'Ставим шрифты'
+pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono 
+
+echo 'Ставим сеть'
+pacman -S networkmanager network-manager-applet ppp --noconfirm
+
+echo 'Подключаем автозагрузку менеджера входа и интернет'
+systemctl enable NetworkManager
+
+
+echo 'Обновляем grub.cfg'
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Собираем PikAUR'
+wget 'https://aur.archlinux.org/cgit/aur.git/snapshot/pikaur.tar.gz'
+tar zxfv pikaur*
+cd pikaur
+pacman -S --needed base base-devel --noconfirm;yes|makepkg -fsri
+cd ..
+rm -r pikaur*
+
+echo 'Ставим довески'
+pikaur -S google-chrome bash-completion remmina freerdp whatsapp-for-linux viber icq-bin anydesk-bin vk-messenger --noconfirm
+
+
+echo 'Установка завершена! Перезагрузите систему.'
+echo 'Если хотите подключить AUR, установить мои конфиги XFCE, тогда после перезагрзки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
+echo 'wget git.io/arch3.sh && sh arch3.sh'
+exit
