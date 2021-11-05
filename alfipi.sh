ac38bdc267fb0e16478d4cb2441df05f4a25f0f7
CryZFix <cryzfix@gmail.com>
2021-11-05 14:31:49 +0400

    Update alfipi.sh

--- alfipi.sh
+++ alfipi.sh
@@ -46,6 +46,16 @@ elif [[ $node_set == 0 ]]; then
   echo 'Пропускаем.'
 fi
 
+echo 'Качаем и устанавливаем настройки Xfce'
+  # Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username 
+  # открйте в этой категории терминал и выполните команду ниже
+  # Предварительно можно очистить конфиг от всего лишнего
+  # tar -czf config.tar.gz .config
+  # Выгрузите архив в интернет и скорректируйте ссылку на свою.
+wget https://github.com/ordanax/arch/raw/master/attach/config.tar.gz
+sudo rm -rf ~/.config/xfce4/*
+sudo tar -xzf config.tar.gz -C ~/
+
 # Подключаем zRam
 pikaur -S zramswap --noconfirm
 sudo systemctl enable zramswap.service
