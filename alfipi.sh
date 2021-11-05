107a01b6c292a88378529e4ae7db6f6e737a237a
CryZFix <cryzfix@gmail.com>
2021-11-05 13:17:36 +0400

    Update alfipi.sh

--- alfipi.sh
+++ alfipi.sh
@@ -21,55 +21,16 @@ echo 'Установка базовых программ и пакетов'
 sudo pacman -S reflector ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
 
 echo 'Установить рекомендумые программы?'
-read -p "1 - Да, 0 - Нет: " prog_set
-if [[ $prog_set == 1 ]]; then
-  #Можно заменить на pacman -Qqm > ~/.pacmanlist.txt
-  pikaur -S recoll google-chrome flameshot veracrypt vlc vim freemind icq-bin anydesk-bin libreoffice libreoffice-fresh-ru neofetch qbittorrent galculator --noconfirm
-  pikaur -Syy
-  pikaur -S xflux hunspell-ru pamac-aur-git megasync-nopdfium trello xorg-xkill ttf-symbola ttf-clear-sans --noconfirm
-elif [[ $prog_set == 0 ]]; then
-  echo 'Установка программ пропущена.'
-fi
-
-echo 'Скачать и установить конфиг и темы для XFCE?'
-read -p "1 - Да, 0 - Нет: " xfce_set
-if [[ $xfce_set == 1 ]]; then
-  echo 'Качаем и устанавливаем настройки Xfce'
-  # Чтобы сделать копию ваших настоек перейдите в домашнюю директорию ~/username 
-  # открйте в этой категории терминал и выполните команду ниже
-  # Предварительно можно очистить конфиг от всего лишнего
-  # tar -czf config.tar.gz .config
-  # Выгрузите архив в интернет и скорректируйте ссылку на свою.
-  echo 'Ставим лого ArchLinux в меню'
-  wget git.io/arch_logo.png
-  sudo mv -f ~/downloads/arch_logo.png /usr/share/pixmaps/arch_logo.png
-  
-  echo 'Удаляем лишнее из xfce4'
-  sudo pacman -Rs xfburn orage parole mousepad xfce4-appfinder xfce4-clipman-plugin xfce4-timer-plugin xfce4-time-out-plugin xfce4-artwork xfce4-taskmanager xfce4-smartbookmark-plugin xfce4-sensors-plugin xfce4-notes-plugin xfce4-netload-plugin xfce4-dplugin xfce4-mpc-plugin xfce4-mount-plugin xfce4-mailwatch-plugin xfce4-genmon-plugin xfce4-fsguard-plugin xfce4-eyes-pluiskperf-plugin xfce4-dict xfce4-cpugraph-plugin xfce4-cpufreq-plugin
-
-  echo 'Ставим обои на рабочий стол'
-  wget git.io/bg.jpg
-  sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартные обои
-  sudo mv -f ~/downloads/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
-elif [[ $xfce_set == 0 ]]; then
-  echo 'Установка конфигов XFCE пропущена.'
-fi 
-
-echo 'Установить conky?'
-read -p "1 - Да, 0 - Нет: " conky_set
-if [[ $conky_set == 1 ]]; then
-  sudo pacman -S conky conky-manager --noconfirm
-  wget git.io/conky.tar.gz
-  tar -xzf conky.tar.gz -C ~/
-elif [[ $conky_set == 0 ]]; then
-  echo 'Установка conky пропущена.'
-fi
+pikaur -S recoll google-chrome flameshot veracrypt vlc vim freemind icq-bin anydesk-bin libreoffice libreoffice-fresh-ru neofetch qbittorrent galculator --noconfirm
+pikaur -Syy
+pikaur -S xflux hunspell-ru pamac-aur-git megasync-nopdfium trello xorg-xkill ttf-symbola ttf-clear-sans nomachine --noconfirm
+sudo pacman -Rs xfburn orage parole mousepad xfce4-appfinder xfce4-clipman-plugin xfce4-timer-plugin xfce4-time-out-plugin xfce4-artwork xfce4-taskmanager xfce4-smartbookmark-plugin xfce4-sensors-plugin xfce4-notes-plugin xfce4-netload-plugin xfce4-dplugin xfce4-mpc-plugin xfce4-mount-plugin xfce4-mailwatch-plugin xfce4-genmon-plugin xfce4-fsguard-plugin xfce4-eyes-pluiskperf-plugin xfce4-dict xfce4-cpugraph-plugin xfce4-cpufreq-plugin
 
 echo 'Делаем авто вход без DE?'
 read -p "1 - Да, 0 - Нет: " node_set
 if [[ $node_set == 1 ]]; then
-sudo systemctl disable lxdm
-sudo pacman -R lxdm
+sudo systemctl disable sddm
+sudo pacman -R sddm
 sudo pacman -S xorg-xinit --noconfirm
 cp /etc/X11/xinit/xserverrc ~/.xserverrc
 wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
@@ -77,9 +38,6 @@ sudo mv -f .xinitrc ~/.xinitrc
 wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.bashrc
 rm ~/.bashrc
 sudo mv -f .bashrc ~/.bashrc
-wget https://raw.githubusercontent.com/ordanax/arch/master/attach/grub
-sudo mv -f grub /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 read -p "Введите имя пользователя: " username
 sudo echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin' "$username" '--noclear %I $TERM' > ~/downloads/override.conf
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
