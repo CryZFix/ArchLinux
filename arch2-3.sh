79e28968e58a6fb945b7d4b0422acf69458f9f80
CryZFix <cryzfix@gmail.com>
2021-12-28 00:41:18 +0400

    Create MinimalServer Install

---
+++ arch2-3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+
+ln -svf /usr/share/zoneinfo/Europe/Samara /etc/localtime
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+
+locale-gen
+
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+
+echo 'KEYMAP=ru' >> /etc/vconsole.conf
+echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+
+mkinitcpio -p linux
+
+pacman-key --init
+pacman-key --populate
+pacman -Syy
+pacman -S grub --noconfirm 
+grub-install /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable sshd
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+
+echo 'Создаем root пароль'
+passwd
+
+echo 'Установка системы завершена! Перезагрузитесь вводом: reboot.'
+exit
