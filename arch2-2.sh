4eb1924d01f94274a1c1ea7392bf1b19d38dd69b
CryZFix <cryzfix@gmail.com>
2022-01-06 21:38:44 +0400

    Update arch2-2.sh

--- arch2-2.sh
+++ arch2-2.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+echo 'Прописываем имя компьютера'
+read -p "Введите имя компьютера: " hostname
+read -p "Введите имя пользователя: " username
+echo $hostname > /etc/hostname
 ln -svf /usr/share/zoneinfo/Europe/Samara /etc/localtime
+
+echo 'Добавляем пользователя'
+useradd -m -g users -G wheel -s /bin/bash $username
+
+echo 'Создаем root пароль'
+passwd
+
+echo 'Устанавливаем пароль пользователя'
+passwd $username
 
 echo '3.4 Добавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
@@ -31,22 +44,44 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo 'Ставим программу для Wi-fi'
 pacman -S dialog wpa_supplicant netctl --noconfirm 
 
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
 echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 pacman -Syy
-pacman -S wget tar bash-completion --noconfirm
+
+echo 'Ставим иксы и драйвера'
+pacman -S wget tar xorg-server xorg-drivers xorg-xinit pulseaudio pavucontrol bash-completion --noconfirm
 
 echo 'Ставим шрифты'
 pacman -S ttf-liberation ttf-dejavu --noconfirm 
 
+echo 'Ставим сеть'
+pacman -S openssh --noconfirm
+
 echo 'Подключаем автозагрузку менеджера входа и интернет'
-systemctl enable dhcpcd
+systemctl enable NetworkManager
 systemctl enable sshd
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
-echo 'Создаем root пароль'
-passwd
+echo 'Делаем авто вход без DE?'
+read -p "1 - Да, 0 - Нет: " node_set
+if [[ $node_set == 1 ]]; then
+sudo systemctl disable sddm
+sudo pacman -S xorg-xinit --noconfirm
+cp /etc/X11/xinit/xserverrc /home/$username/.xserverrc
+wget https://raw.githubusercontent.com/ordanax/arch/master/attach/.xinitrc
+sudo mv -f .xinitrc /home/$username/.xinitrc
+sudo echo -e '[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin' "$username" '--noclear %I $TERM' > override.conf
+sudo mkdir /etc/systemd/system/getty@tty1.service.d/
+sudo mv -f override.conf /etc/systemd/system/getty@tty1.service.d/override.conf
+elif [[ $node_set == 0 ]]; then
+  echo 'Пропускаем.'
+fi
+
+rm -rf downloads
 
 echo 'Установка системы завершена! Перезагрузитесь вводом: reboot.'
 exit
