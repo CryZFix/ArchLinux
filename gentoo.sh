bb9e06b57a682b4f51320d38cd629e74ecb5f1d9
CryZFix <cryzfix@gmail.com>
2022-04-02 22:10:03 +0400

    Update

--- gentoo.sh
+++ gentoo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+loadkeys ru
+setfont cyr-sun16
+
 read -p "Введите дату и время в форамате MMDDhhmmYYYY: " datatime
 date $datatime
 
@@ -9,4 +12,13 @@ links 'https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/'
 
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 
+read -p "Введите колличество потоков вашего процессора(не ядер, именно потоков): " cputhreads
+echo MAKEOPTS="$cputhreads" >> /mnt/gentoo/etc/portage/make.conf
+
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys 
+mount --make-rslave /mnt/gentoo/sys 
+mount --rbind /dev /mnt/gentoo/dev 
+mount --make-rslave /mnt/gentoo/dev
