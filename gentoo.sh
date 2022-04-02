34206e446309cfbe5e20a05a2a293b187a311d56
CryZFix <cryzfix@gmail.com>
2022-04-02 23:05:32 +0400

    added disk partitioning

--- gentoo.sh
+++ gentoo.sh
@@ -3,16 +3,55 @@
 loadkeys ru
 setfont cyr-sun16
 
-read -p "Введите дату и время в форамате MMDDhhmmYYYY: " datatime
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo '2.4 создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +2048M;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +4096M;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo;
+
+  echo w;
+) | fdisk /dev/sda
+
+echo '2.4.2 Форматирование дисков'
+mkfs.ext2  /dev/sda1 -L boot
+mkfs.ext4  /dev/sda3 -L root
+mkswap /dev/sda2 -L swap
+
+echo '2.4.3 Монтирование дисков'
+mount /dev/sda3 /mnt/gentoo
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/gentoo/boot
+swapon /dev/sda2
+
+read -p "Enter the date and time in the format > MMDDhhmmYYYY: " datatime
 date $datatime
 
 cd /mnt/gentoo
 
-links 'https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/'
+links 'https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-openrc/'
 
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 
-read -p "Введите колличество потоков вашего процессора(не ядер, именно потоков): " cputhreads
+read -p "Enter the number of threads on your processor (not cores, just threads): " cputhreads
 echo MAKEOPTS="$cputhreads" >> /mnt/gentoo/etc/portage/make.conf
 
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
