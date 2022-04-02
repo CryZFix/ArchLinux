ce61815542b7b19af787ebab5575a6b8ef47d235
CryZFix <cryzfix@gmail.com>
2022-04-02 22:05:04 +0400

    Update

--- gentoo.sh
+++ gentoo.sh
@@ -1,50 +1,5 @@
 #!/bin/bash
 
-loadkeys ru
-setfont cyr-sun16
-
-echo '2.3 Синхронизация системных часов'
-timedatectl set-ntp true
-
-echo '2.4 создание разделов'
-(
-  echo o;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +2048M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +8192M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo;
-
-  echo w;
-) | fdisk /dev/sda
-
-echo 'Ваша разметка диска'
-fdisk -l
-
-echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L boot
-mkfs.ext4  /dev/sda3 -L root
-mkswap /dev/sda2 -L swap
-
-echo '2.4.3 Монтирование дисков'
-mount /dev/sda3 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda2
-
 read -p "Введите дату и время в форамате MMDDhhmmYYYY: " datatime
 date $datatime
 
