24f11317b947c13e5dd5113e361507393953c163
CryZFix <cryzfix@gmail.com>
2022-04-03 14:44:51 +0400

    Add /run mount

--- gentoo.sh
+++ gentoo.sh
@@ -60,6 +60,8 @@ mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys 
 mount --rbind /dev /mnt/gentoo/dev 
 mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run
 
 chroot /mnt/gentoo sh -c "$(curl -fsSL https://raw.githubusercontent.com/CryZFix/ArchLinux/main/gentoo2.sh)"
 
