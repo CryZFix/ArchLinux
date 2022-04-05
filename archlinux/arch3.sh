27ff4d465f327491e02ec6190288d509beca34ae
CryZFix <cryzfix@gmail.com>
2022-04-05 22:22:04 +0400

    Update and rename arch2-2.sh to arch3.sh

---
+++ archlinux/arch3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p files
+cd files
+echo 'Установка AUR (yay)'
+sudo pacman-key --init
+sudo pacman-key --populate
+sudo pacman -Syyu wget git curl --needed base base-devel --noconfirm
+wget 'https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz'
+tar xzfv yay*
+cd yay
+makepkg -fsri --noconfirm
+cd ..
+cd ..
+rm -rf files
+
+yay -Syyu i3-gaps polybar rofi pywal calc networkmanager-dmenu zramswap --noconfirm
+sudo systemctl enable zramswap.service
+
+exit
