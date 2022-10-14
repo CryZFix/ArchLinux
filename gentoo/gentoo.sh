5555ed10ad04b875611fa257e1ed7bac48548d4b
CryZFix <cryzfix@gmail.com>
2022-10-14 05:12:01 +0400

    merge scripts into one

--- gentoo/gentoo.sh
+++ gentoo/gentoo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo 'Partition BIOS or UEFI'
-read -p "1 - BIOS, 2 - UEFI: " node_set
-if [[ $node_set == 1 ]]; then
+read -p "Enter the number of threads on your processor (not cores, just threads): " cputhreads
+
 echo 'Creating partitions'
 (
   echo o;
@@ -32,60 +31,30 @@ echo 'Formatting disks'
 mkfs.ext2 /dev/sda1 -L boot
 mkswap /dev/sda2 -L swap
 mkfs.ext4 /dev/sda3 -L root
-elif [[ $node_set == 2 ]]; then
-echo 'Creating partitions'
-(
-  echo g;
-
-  echo n;
-  echo;
-  echo;
-  echo +2048M;
-  echo t;
-  echo 1;
-
-  echo n;
-  echo;
-  echo;
-  echo +4096M;
-  echo t;
-  echo 2;
-  echo 19;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo;
-
-  echo w;
-) | fdisk /dev/sda
-
-echo 'Formatting disks'
-mkfs.vfat -F 32 /dev/sda1 -L boot
-mkswap /dev/sda2 -L swap
-mkfs.ext4 /dev/sda3 -L root
-fi
 
 echo 'Mounting disks'
+mkdir -p /mnt/gentoo
 mount /dev/sda3 /mnt/gentoo
 mkdir /mnt/gentoo/boot
 mount /dev/sda1 /mnt/gentoo/boot
 swapon /dev/sda2
 
-read -p "Enter the date and time in the format > MMDDhhmmYYYY: " datatime
-date $datatime
-
 cd /mnt/gentoo
 
-echo 'Select the desired stage3 -- tar.xz'
-read -n 1 -s -r -p "Press any key to continue"
-links 'https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64-openrc/'
+DISTMIRROR=http://distfiles.gentoo.org
+DISTBASE=${DISTMIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/
+FILE=$(wget -q $DISTBASE -O - | grep -o -E 'stage3-amd64-openrc-20\w*\.tar\.(bz2|xz)' | uniq)
+[ -z "$FILE" ] && echo No stage3 found on $DISTBASE && exit 1
+echo download latest stage file $FILE
+wget $DISTBASE$FILE 
 
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+rm stage3-*
+curl -OL https://raw.githubusercontent.com/CryZFix/Linux/main/gentoo/genfstab
+chmod +x genfstab
+./genfstab /mnt/gentoo > /mnt/gentoo/etc/fstab
 
 sed -i 's/COMMON_FLAGS="-O2 -pipe"/COMMON_FLAGS="-march=native -O2 -pipe"/' /mnt/gentoo/etc/portage/make.conf
-read -p "Enter the number of threads on your processor (not cores, just threads): " cputhreads
 echo MAKEOPTS='"-j'$cputhreads'"' >> /mnt/gentoo/etc/portage/make.conf
 
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
@@ -101,10 +70,90 @@ mount --make-rslave /mnt/gentoo/dev
 mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
-chroot /mnt/gentoo sh -c "$(curl -fsSL https://raw.githubusercontent.com/CryZFix/linux/main/gentoo/gentoo2.sh)"
+# Make pre-configure file installer
+cat > chrootstart.sh << EOF
+#!/bin/bash
 
-cd
-umount -l /mnt/gentoo/dev{/shm,/pts,} 
-umount -R /mnt/gentoo
+hostname=reichstag
+username=junker
+password=123456
+
+echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
+
+source /etc/profile
+export PS1="(chroot) $PS1"
+
+emerge-webrsync
+
+emerge -qvuDN @world
+emerge cpuid2cpuflags
+echo "CPU_FLAGS_X86=$(cpuid2cpuflags | grep -oP ': \K.*')" | sed 's/=/="/;s/$/"/' >> /etc/portage/make.conf
+echo 'INPUT_DEVICES="synaptics libinput"' >> /etc/portage/make.conf
+
+# graphics driver
+nvidia=$(lspci | grep -e VGA -e 3D | grep 'NVIDIA' 2> /dev/null || echo '')
+amd=$(lspci | grep -e VGA -e 3D | grep 'AMD' 2> /dev/null || echo '')
+intel=$(lspci | grep -e VGA -e 3D | grep 'Intel' 2> /dev/null || echo '')
+if [[ -n "$nvidia" ]]; then
+  echo 'VIDEO_CARDS="nouveau"' >> /etc/portage/make.conf
+fi
+
+if [[ -n "$amd" ]]; then
+  echo 'VIDEO_CARDS="amdgpu radeon radeonsi"' >> /etc/portage/make.conf
+fi
+
+if [[ -n "$intel" ]]; then
+  echo 'VIDEO_CARDS="intel"' >> /etc/portage/make.conf
+fi
+
+echo "Europe/Samara" > /etc/timezone
+emerge --config sys-libs/timezone-data
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+echo 'ru_RU.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+eselect locale list
+eselect locale set 4
+
+sed -i 's/CONSOLEFONT="default8x16"/CONSOLEFONT="cyr-sun16"/' /etc/conf.d/consolefont
+env-update && source /etc/profile
+export PS1="(chroot) $PS1"
+
+emerge -q sys-kernel/gentoo-sources sys-kernel/genkernel sys-fs/e2fsprogs sys-fs/btrfs-progs sys-fs/dosfstools dhcpcd
+rc-update add dhcpcd default
+
+eselect kernel list
+eselect kernel set 1
+
+genkernel all
+
+echo hostname="$sethostname" > /etc/conf.d/hostname
+
+useradd -m -G wheel,audio,video $username
+echo "$username:$password" | chpasswd
+
+emerge -q sys-boot/grub:2
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+emerge x11-base/xorg-drivers x11-base/xorg-server git alacritty
+git clone https://github.com/bakkeby/dwm-flexipatch.git
+cd dwm-flexipatch
+make install
+
+echo 'exec dwm &'
+
+EOF
+
+time chroot . ./chrootstart.sh
+rm chrootstart.sh
+
+umount var/tmp
+rm -rf var/tmp/*
+rm -rf var/cache/distfiles
+umount *
+cd /
+## umount somehow fails recently, but can not find usage, lets go lazy
+umount -l /mnt/gentoo  || exit 1
 
 echo 'Installation was complity. You can reboot PC..'
