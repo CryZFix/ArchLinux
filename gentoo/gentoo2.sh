eb8f28ef50841795758d6faee870f698cef21254
CryZFix <cryzfix@gmail.com>
2022-10-14 13:38:01 +0000

    .

--- gentoo/gentoo2.sh
+++ gentoo/gentoo2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+hostname=reichstag
+username=junker
 
 echo 'ACCEPT_LICENSE="*"' >> /etc/portage/make.conf
 
@@ -30,35 +32,16 @@ export PS1="(chroot) $PS1"
 
 emerge -q sys-kernel/gentoo-kernel-bin sys-fs/e2fsprogs sys-fs/btrfs-progs sys-fs/dosfstools dhcpcd
 rc-update add dhcpcd default
-echo '/dev/sda1         /boot       ext2        defaults                0 2' > /etc/fstab
+
 eselect kernel list
 eselect kernel set 1
-
-echo 'Kernel config Manual or use genkernel?'
-read -p "1 - Manual, 2 - Genkernel: " node_set
-if [[ $node_set == 1 ]]; then
-cd /usr/src/linux
-make menuconfig
-make -j9 bzImage
-make -j9 modules
-make install
-make INSTALL_MOD_STRIP=1 modules_install
-elif [[ $node_set == 2 ]]; then
 genkernel all
-fi
 
-echo '/dev/sda2         none        swap        sw                      0 0' >> /etc/fstab
-echo '/dev/sda3         /           ext4        noatime                 0 1' >> /etc/fstab
-
-read -p "Enter name your PC: " sethostname
-echo hostname="$sethostname" > /etc/conf.d/hostname
-
-read -p "Enter username: " username
+echo hostname="$hostname" > /etc/conf.d/hostname
 useradd -m -G wheel,audio,video $username
-read -n 1 -s -r -p "Press any key to continue and type password for user: $username"
-passwd $username
-read -n 1 -s -r -p "Press any key to continue and type password for root user"
-passwd
+### IF NOT SET_PASS is set then the password will be "password"
+SET_PASS=${SET_PASS:-password}
+echo "$username:${SET_PASS}" | chpasswd
 
 emerge -q sys-boot/grub:2
 
