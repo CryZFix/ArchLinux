6f2932f278c076ff1f805602a4403edf3bdd2ff3
CryZFix <cryzfix@gmail.com>
2023-02-10 13:28:36 +0400

    Change AUR-Helper yay to pikaur

--- archlinux/arch3.sh
+++ archlinux/arch3.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 username=junker
 
-APPS='google-chrome polybar calc networkmanager-dmenu-git neovim-plug'
+APPS='google-chrome polybar calc networkmanager-dmenu-git'
 
 cd /home/$username
 mkdir -p files
 cd files
-echo 'Установка AUR (yay)'
+echo 'Установка AUR (pikaur)'
 sudo pacman-key --init
 sudo pacman-key --populate
 sudo pacman -Syyu wget git curl --needed base base-devel --noconfirm
-wget 'https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz'
-tar xzfv yay*
-cd yay
+wget 'https://aur.archlinux.org/cgit/aur.git/snapshot/pikaur.tar.gz'
+tar xzfv pikaur*
+cd pikaur
 makepkg -fsri --noconfirm
 cd ..
 cd ..
 rm -rf files
 
-yay -Syyu $APPS --noconfirm
+pikaur -Syyu $APPS --noconfirm
